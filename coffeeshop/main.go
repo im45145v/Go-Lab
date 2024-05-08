a4711277c0325a4777ecbb1b98a40ba231349a7a
ashish malla <74368820+im45145v@users.noreply.github.com>
2024-05-08 03:46:13 +0000

    trying to list all the coffee things that exists

---
+++ coffeeshop/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+        "fmt"
+        "coffeeshop/coffee"
+)
+func main() {
+        coffees, err := coffee.GetCoffees()
+        if err != nil {
+		fmt.Println("Error while getting coffeelist ", err)
+		return
+	}
+
+	fmt.Println("Printing the list of coffees available in the CoffeeShop...")
+        for _, element := range coffees.List {
+                result := fmt.Sprintf("%s for $%v", element.Name, element.Price)
+                fmt.Println(result)
+        }
+}
