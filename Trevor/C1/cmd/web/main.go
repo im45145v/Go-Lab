30de7a90f6ec60beb087e84ef2c180ed2b7d1041
ashish malla <74368820+im45145v@users.noreply.github.com>
2024-06-17 13:32:31 +0000

    New Course started

---
+++ Trevor/C1/cmd/web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"C1/pkg/handlers"
+	"fmt"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// main is the main function
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
---
+++ Trevor/C1/pkg/handlers/handlers.go
@@ -0,0 +1,16 @@
+package handlers
+
+import (
+	"C1/pkg/render"
+	"net/http"
+)
+
+// Home is the handler for the home page
+func Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.tmpl")
+}
+
+// About is the handler for the about page
+func About(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "about.page.tmpl")
+}
